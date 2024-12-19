72a0423779c6c0dbd6b8009b297ad74864b7f584
clearxheaded <7777x777x7777@gmail.com>
2024-12-19 14:42:31 -0500

    Add a hover effect using js
    - Added a hover effect for square divs

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector("#container");
     for(let i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.classList.add("square");
     container.appendChild(div);
-}
+    }
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "blue";
+        })
+    });
 });
